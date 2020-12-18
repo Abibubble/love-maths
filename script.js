e7806a12f75cffff3d75581031c799a877c5cf83
Abibubble <abi.yamaha.neo@gmail.com>
2020-12-18 20:30:09 +0000

    feat: Create functionality for addition

--- script.js
+++ script.js
@@ -10,13 +10,15 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
-function runGame() {
+function runGame(gameType) {
 
     // Generate two random rumbers between 1 and 25
     // Math.floor rounds down to the whole number
@@ -24,6 +26,13 @@ function runGame() {
 
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type ${gameType}`);
+        throw `Unknown game type ${gameType}, aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -42,8 +51,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-    Math.floor(Math.random() * 25) + 1;
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuestion() {
