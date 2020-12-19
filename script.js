a52b09a6f0e212e6b3626b0278406dfd9dd45ff1
Abibubble <abi.yamaha.neo@gmail.com>
2020-12-19 21:40:35 +0000

    feat: Create score and incorrect answer functions

--- script.js
+++ script.js
@@ -46,8 +46,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :)");
+        incrementScore();
     } else {
-        alert(`Awww... You answered ${userAnswer}, the correct answer was ${calculatedAnswer[0]}!`)
+        alert(`Awww... You answered ${userAnswer}, the correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -79,9 +81,19 @@ function calculateCorrectAnswer() {
 
 function incrementScore() {
 
+    // Gets the current score from the DOM and increments it
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+
 }
 
 function incrementWrongAnswer() {
+
+    // Gets the current tally of incorrect answers from the DOM and increments it
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
